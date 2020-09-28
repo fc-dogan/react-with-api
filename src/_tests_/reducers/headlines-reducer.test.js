8687716f912ebb813f07cecec9b20d16d9d4b0cd
Fatma <fceyda@gmail.com>
2020-09-28 12:53:12 -0700

    add test for get_headlines_success

--- src/_tests_/reducers/headlines-reducer.test.js
+++ src/_tests_/reducers/headlines-reducer.test.js
@@ -29,4 +29,18 @@ describe('headlinesReducer', () => {
         error: null
     });
   });
+
+  test('successfully getting headlines should change isLoading to false and update headlines', () => {
+    const headlines = "A headline";
+    action = {
+      type: c.GET_HEADLINES_SUCCESS,
+      headlines
+    };
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+        isLoading: false,
+        headlines: "A headline",
+        error: null
+    });
+  });
 });
