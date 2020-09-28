0e1e69ae89536bda6b7ff69ec4e808391ced1a0c
Fatma <fceyda@gmail.com>
2020-09-28 12:51:42 -0700

    pass test for request_headlines

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Headlines from'./Headlines'
+
+
+function App() {
+  return (
+    <div className="App">
+      <React.Fragment>
+      < Headlines />
+    </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
