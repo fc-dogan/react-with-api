dfd5df73560196bdae7366ad23073c3b5c019678
Fatma <fceyda@gmail.com>
2020-09-28 13:54:23 -0700

    update headline js to connect redux

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { makeApiCall } from '../actions';
 
 class Headlines extends React.Component {
   constructor(props) {
@@ -29,11 +31,13 @@ class Headlines extends React.Component {
   // }
 
   componentDidMount() {
-    this.makeApiCall()
+    // Now we'll use dispatch() to make our API call.
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
 
   render() {
-  const { error, isLoaded, headlines } = this.state;
+  const { error, isLoaded, headlines } = this.props;
   if (error) {
     return <React.Fragment>Error: {error.message}</React.Fragment>;
   } else if (!isLoaded) {
@@ -56,4 +60,12 @@ class Headlines extends React.Component {
 }
 }
 
-export default Headlines;
+const mapStateToProps = state => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Headlines);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers/headlines-reducer';
 import { Provider } from 'react-redux';
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
